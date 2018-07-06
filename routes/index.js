ba651b92a140b9ac3472fe15f028f0ec1f5b0265
Gustavo Aguiar <gugahoa@gmail.com>
2018-07-06 18:08:47 -0300

    Register bills and products endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const user = require('./user');
 const pets = require('./pets');
 const bookings = require('./bookings');
+const products = require('./products');
+const bills = require('./bills');
 
 module.exports = {
     '/users': user,
     '/pets': pets,
-    '/bookings': bookings
+    '/bookings': bookings,
+    '/products': products,
+    '/bills': bills
 };
